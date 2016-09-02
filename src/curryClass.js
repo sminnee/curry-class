ba0aec1997bc3d845c2fdadb3e5a7573bb2f268d
Sam Minnee <sam@silverstripe.com>
2016-09-02 15:35:26 +1200

    Add styleguide linting.

--- src/curryClass.js
+++ src/curryClass.js
@@ -1,22 +1,20 @@
-module.exports = function (SourceClass) {
+module.exports = function curryClass(SourceClass) {
+  var curriedArgs = Array.prototype.slice.call(arguments, 1);
 
-	var curriedArgs = Array.prototype.slice.call(arguments, 1);
+  return function curriedConstructor() {
+    var combinedArgs = curriedArgs.concat(Array.prototype.slice.call(arguments, 0));
 
-	return function() {
-		var combinedArgs = curriedArgs.concat(Array.prototype.slice.call(arguments, 0));
+    // Create an object that inherits from `proto`
+    var hasOwnProto = Object(SourceClass.prototype) === SourceClass.prototype;
+    var obj = Object.create(hasOwnProto ? SourceClass.prototype : Object.prototype);
 
-		// Create an object that inherits from `proto`
-		var obj = Object.create(
-			Object(SourceClass.prototype) === SourceClass.prototype ? SourceClass.prototype : Object.prototype
-		);
+    // Apply the function setting `obj` as the `this` value
+    var ret = SourceClass.apply(obj, combinedArgs);
 
-		// Apply the function setting `obj` as the `this` value
-		var ret = SourceClass.apply(obj, combinedArgs);
+    if (Object(ret) === ret) { // the result is an object?
+      return ret;
+    }
 
-		if (Object(ret) === ret) { // the result is an object?
-			return ret;
-		}
-
-		return obj;
-	}
-}
+    return obj;
+  };
+};
--- tests/curryClass.js
+++ tests/curryClass.js
@@ -1,30 +1,26 @@
 var test = require('tape');
 var curryClass = require('../src/curryClass');
 
-test('curryClass', function(t) {
-  ///////////////
+function MyClass() {
+  this.argList = Array.prototype.slice.apply(arguments, [0]);
+}
 
-  function MyClass(arg1, arg2) {
-    this.argList = Array.prototype.slice.apply(arguments, [0]);
-  }
-  MyClass.prototype.result = function() {
-    return this.argList;
-  }
-
-  var x = new MyClass('hello', 'world');
-  t.deepEquals(x.result(), ['hello', 'world'])
-
+test('curryClass', function curryClassTest(t) {
   var CurriedClass = curryClass(MyClass, 'goodbye');
-  var y = new CurriedClass('now');
-  t.deepEquals(y.result(), ['goodbye', 'now'])
-
   var CurriedClass2 = curryClass(MyClass, 'one', 'two');
-  var y = new CurriedClass2();
-  t.deepEquals(y.result(), ['one', 'two'])
-
   var CurriedClass3 = curryClass(MyClass, 'one', 'two');
-  var y = new CurriedClass3('three', 'four');
-  t.deepEquals(y.result(), ['one', 'two', 'three', 'four'])
+
+  MyClass.prototype.result = function result() {
+    return this.argList;
+  };
+
+  t.deepEquals((new MyClass('hello', 'world')).result(), ['hello', 'world']);
+
+  t.deepEquals((new CurriedClass('now')).result(), ['goodbye', 'now']);
+
+  t.deepEquals((new CurriedClass2()).result(), ['one', 'two']);
+
+  t.deepEquals((new CurriedClass3('three', 'four')).result(), ['one', 'two', 'three', 'four']);
 
   t.end();
 });
